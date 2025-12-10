30989a83f200e116171e27e3b22a7c2ebeef7901
vereyutinam <vereyutinam@gmail.com>
2025-12-10 06:16:03 +0300

    Create Main.cpp

---
+++ lab5/Main.cpp
@@ -0,0 +1,14 @@
+#include "MainForm.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew MainForm());
+    return 0;
+}
